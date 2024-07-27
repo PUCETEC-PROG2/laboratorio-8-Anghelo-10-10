c0c0013cbc536e5d67a809195329df051f8f1475
anghelo-10 <anghelomontiel10@gmail.com>
2024-07-26 23:17:50 -0500

    Modificado

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("pokemon/<int:pokemon_id>/", views.pokemon, name="pokemon"),
     path("add_pokemon/", views.add_pokemon, name="add_pokemon"),
     path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("pokemon/edit/<int:id>/", views.edit_pokemon, name="edit_pokemon"),
+    path("pokemon/delete/<int:id>/", views.edit_pokemon, name="edit_pokemon"),
 ]
+
